f68d2d6ba9e5edbe6a2e8d115a5fd5cd329fa01c
HolaJun <ljh_0107@naver.com>
2018-08-28 23:54:22 +0900

    test

--- public/js/test.js
+++ public/js/test.js
@@ -1,15 +1,27 @@
 
+// 간단 가계부 함수
+
 var amounts = []; // 금액 기록
 var names = []; // 월급, 용돈... 명목 기록
 var total = 0;
 
 function deposit(amount, name) {
-  amounts[] = amount;
-
+  if(amount + total < 0) {
+    throw new Error('Not enough balance');
+  }
+  amounts.push(amount);
+  names.push(name);
+  total += amount;
 }
 
 function print() {
-
+  for(var i=0; i<amounts.length; i++) {
+    var amount = amounts[i];
+    var name = names[i];
+    var isDeposit = amount > 0;
+    console.log(`${isDeposit ? '입금' : '출금'}\t${name}\t${amount}\n`);
+  }
+  console.log(`잔액:\t${total}`);
 }
 
 // 가계부
