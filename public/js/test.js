d44ce06ef8555a9e0865b0b23cc81d588d73f7e7
HolaJun <ljh_0107@naver.com>
2018-08-28 23:46:16 +0900

    test

--- public/js/test.js
+++ public/js/test.js
@@ -1,9 +1,25 @@
 
-function inner() {
-  var a = 10;
-  a = 30;
-  b = 20;
+var amounts = []; // 금액 기록
+var names = []; // 월급, 용돈... 명목 기록
+var total = 0;
+
+function deposit(amount, name) {
+  amounts[] = amount;
+
 }
 
-console.log(b);
-console.log(a);
+function print() {
+
+}
+
+// 가계부
+try {
+  deposit(100, "월급");
+  deposit(200, "용돈");
+  deposit(-150, "월세");
+  deposit(-300, "보험료");
+} catch (e) {
+  console.log(e);
+}
+
+print();
